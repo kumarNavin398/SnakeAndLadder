145390d0586f77c2a9928b432749f7952e18b932
KumarNavin <navins398@gmail.com>
2023-01-03 23:38:10 +0530

    uc2 completed

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,20 +1,15 @@
 package com.bridgelabz;
 
-
 //Day 4 Snake And Ladder Simulator
 public class SnakeAndLadder
 {
-    public static int getRandomInteger(int maximum, int minimum){
-        return ((int) (Math.random()*(maximum - minimum))) + minimum;
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
 
         int startPositionA = 0;
-        int dice = getRandomInteger(7,1);
-        System.out.println(dice);
 
     }
 }
+
